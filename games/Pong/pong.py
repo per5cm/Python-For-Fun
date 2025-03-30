b6987e9792cb9017e20708adb91ce57fa6602ad6
per5cm <per5cm@proton.me>
2025-03-30 19:23:12 +0200

    ball movement

--- games/Pong/pong.py
+++ games/Pong/pong.py
@@ -33,6 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2         #ball d stands for delta and its coords x,y how fast ball is moving on the screen
+ball.dy = 2
 
 # Function
 def paddle_a_up():
@@ -65,3 +67,7 @@ wn.onkeypress(paddle_b_down, "Down")
 # Main game loop
 while True:
     wn.update()
+    
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
