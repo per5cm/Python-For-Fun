d07b7471240ee68f1d273fc3df33f444970f3a46
per5cm <per5cm@proton.me>
2025-03-31 23:26:03 +0200

    update scraper

--- forFun/web_scraping/main.py
+++ forFun/web_scraping/main.py
@@ -10,3 +10,8 @@ page = requests.get(url)
 soup = BeautifulSoup(page.text, "html")
 
 print(soup)
+
+soup.find_all("table", class_ = "wikitable sortable jquery-tablesorter")[1]
+
+#<table class="wikitable sortable jquery-tablesorter">
+#<caption>
