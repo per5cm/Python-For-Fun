c9d59bd3e581bb7311ae1b0a2928830d77392ad1
per5cm <per5cm@proton.me>
2025-03-31 17:33:59 +0200

    web scaper

---
+++ forFun/web_scraping/main.py
@@ -0,0 +1,12 @@
+# Web Scraper
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue"
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, "html")
+
+print(soup)
